e3a76910ae3be87b8959bd09571acc4f88800de8
lidergang789-png <lidergang789@gmail.com>
2026-08-01 19:44:28 +0600

    Update emojiVoice.js

--- Script/commands/emojiVoice.js
+++ Script/commands/emojiVoice.js
@@ -45,7 +45,7 @@ const emojiAudioMap = {
  "😩": "https://files.catbox.moe/b4m5aj.mp3",
  "🫣": "https://files.catbox.moe/ttb6hi.mp3",
  "🐸": "https://files.catbox.moe/utl83s.mp3",
- "🙂": "https://files.catbox.moe/ucf3kw.m4a"
+ "🙂": "https://files.catbox.moe/fm23hc.mp4"
 };
 
 module.exports.handleEvent = async ({ api, event }) => {
