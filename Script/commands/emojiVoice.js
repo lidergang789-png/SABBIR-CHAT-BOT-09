3679ff94714ca0ab9f2420b320d4c46c477105ba
lidergang789-png <lidergang789@gmail.com>
2026-08-01 18:13:26 +0600

    Update emojiVoice.js

--- Script/commands/emojiVoice.js
+++ Script/commands/emojiVoice.js
@@ -44,7 +44,7 @@ const emojiAudioMap = {
  "🥹": "https://files.catbox.moe/jf85xe.mp3",
  "😩": "https://files.catbox.moe/b4m5aj.mp3",
  "🫣": "https://files.catbox.moe/ttb6hi.mp3",
- "🐸": "https://files.catbox.moe/utl83s.mp3"
+ "🐸": "https://files.catbox.moe/utl83s.mp3",
  "🙂": "https://files.catbox.moe/ucf3kw.m4a"
 };
 
