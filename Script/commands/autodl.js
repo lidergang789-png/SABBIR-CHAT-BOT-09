9fd13124bb758d00f5b791c6d624e2f72ec66e56
lidergang789-png <lidergang789@gmail.com>
2026-08-02 17:40:43 +0600

    Update autodl.js

--- Script/commands/autodl.js
+++ Script/commands/autodl.js
@@ -29,7 +29,7 @@ const data = await alldown(content);
  fs.writeFileSync(__dirname + "/cache/auto.mp4", Buffer.from(video, "utf-8"))
 
  return api.sendMessage({
- body: `🔥🚀 𝐒𝐡𝐚𝐡𝐚𝐝𝐚𝐭 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭🔥💻 
+ body: `🔥🚀 𝐒abbir 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭🔥💻 
 📥⚡𝗔𝘂𝘁𝗼 𝗗𝗼𝘄𝗻𝗹𝗼𝗮𝗱𝗲𝗿⚡📂
 🎬 𝐄𝐧𝐣𝐨𝐲 𝐭𝐡𝐞 𝐕𝐢𝐝𝐞𝐨 🎀`,
  attachment: fs.createReadStream(__dirname + "/cache/auto.mp4")
